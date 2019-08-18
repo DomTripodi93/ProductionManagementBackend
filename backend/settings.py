5a2718f5db60c154a6f6b5df774e6da72f3f5709
Dominic Tripodi <dtripodi93@gmail.com>
2019-08-17 22:07:24 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", 'k2hdgp^1&mFakeKey90mucu3vht2@tk')
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = [
-    "ec2-3-17-73-216.us-east-2.compute.amazonaws.com", 
+    "ec2-13-58-117-5.us-east-2.compute.amazonaws.com", 
     "myproductionstatus-1686988022.us-east-2.elb.amazonaws.com",
     "127.0.0.1",
     "myproducionstatus.com"
